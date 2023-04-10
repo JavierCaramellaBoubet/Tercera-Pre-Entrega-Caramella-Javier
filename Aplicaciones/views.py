3f69a5d9c9f2f711d6d22d06ca084b8a164c3708
JavierCaramellaBoubet <jacar2000@hotmail.com>
2023-04-10 14:33:18 -0300

    3_PreEntrega_v2_Carrito_de_Compras

--- Aplicaciones/views.py
+++ Aplicaciones/views.py
@@ -2,19 +2,29 @@ from django.shortcuts import render, redirect
 from .models import Compra
 from django.contrib import messages
 
+#import datetime  #Para usar luego en el Día y Hora de la Compra
+#import random    #Para usar luego en la generación del Número de Ticket
+
+
+
+
 # Create your views here.
 
 
 def home(request):
     compraListados = Compra.objects.all()
     messages.success(request, '¡Compras listadas!')
-    return render(request, "GestionCompras.html", {"cursos": compraListados})
+    return render(request, "GestionCompras.html", {"compra": compraListados})
 
 
 def registrarCompra(request):
     codigo = request.POST['txtCodigo']
     nombre = request.POST['txtNombre']
     cantidad = request.POST['numCantidad']
+
+    #dia_compra=datetime.datetime.today()
+    #print(f"Día y Hora de Compra: {dia_compra}")
+    #ticket_numero= random.randrange(0,999999999999)
 
     compra = Compra.objects.create(codigo=codigo, nombre=nombre, cantidad=cantidad)
     messages.success(request,'¡Compra registrada!')
@@ -23,7 +33,7 @@ def registrarCompra(request):
 
 def edicionCompra(request, codigo):
     compra = Compra.objects.get(codigo=codigo)
-    return render(request, "EdicionCompra.html", {"compra": compra})
+    return render(request, "edicionCompra.html", {"compra": compra})
 
 
 def editarCompra(request):
@@ -31,10 +41,10 @@ def editarCompra(request):
     nombre = request.POST['txtNombre']
     cantidad = request.POST['numCantidad']
 
-    cantidad = Compra.objects.get(codigo=codigo)
-    Compra.nombre = nombre
-    Compra.cantidad = cantidad
-    Compra.save()
+    compra = Compra.objects.get(codigo=codigo)
+    compra.nombre = nombre
+    compra.cantidad = cantidad
+    compra.save()
 
     messages.success(request, '¡Compra actualizada!')
 
@@ -52,3 +62,5 @@ def eliminarCompra(request, codigo):
 def vista5(request):
     diccionario={}
     return render(request, "Aplicaciones/templates/base.html", context=diccionario)
+
+
