b54d33ea7b63263bd42e632e76b44433005af1ce
JavierCaramellaBoubet <jacar2000@hotmail.com>
2023-04-28 18:28:13 -0300

    3PreEntrega_con botón de BUSQUEDA y CRUD

--- Aplicaciones/urls.py
+++ Aplicaciones/urls.py
@@ -6,15 +6,15 @@ urlpatterns = [
     #path('', views.home),
     path('', views.inicio),
     path('registrarCompra/', views.registrarCompra, name="registrarCompra"),
-    #path('edicionCompra/<codigo>', views.edicionCompra),
-    #path('editarCompra/', views.editarCompra),
-    #path('eliminarCompra/<codigo>', views.eliminarCompra),
+    path('edicionCompra/<codigo>', views.edicionCompra),
+    path('editarCompra/', views.editarCompra),
+    path('eliminarCompra/<codigo>', views.eliminarCompra),
     
     path('', inicioApp, name="inicioApp"),
     path('contacto/', contacto, name="contacto"),
     path('promociones/', promociones, name="promociones"),
-    #path('crear_usuario/', crear_usuario, name="crear_usuario"),
-    path('crear_usuario/', views.crear_usuario, name="crear_usuario"),    
+
+      path('crear_usuario/', views.crear_usuario, name="crear_usuario"),    
 
     path('buscarProducto/', buscarProducto, name="buscarProducto"),
     path('buscandoProducto/', buscandoProducto, name="buscandoProducto"),
